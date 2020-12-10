2d4dd22b173803ba629709d8c0363a3b5a2348be
Adengbono <67632787+Adengbono@users.noreply.github.com>
2020-12-10 22:37:21 +0000

    Add files via upload
    blog-app-backend so far

---
+++ index.js
@@ -0,0 +1,46 @@
+require ('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+
+const userRoute = require('./src/routes/User')
+const blogRoute = require('./src/routes/blog')
+
+const app = express()
+
+const db = process.env.db
+
+const port = process.env.port
+
+mongoose.connect(db, 
+    {
+    useNewUrlParser: true, 
+    useUnifiedTopology:true, 
+    useCreateIndex: true
+    },
+    ()=>{
+    app.listen(port, () => {
+        //also trying to log info
+        console.info('Application started');
+    })
+})
+//middleware
+app.use(express.json())
+
+//routes
+app.use(userRoute)
+app.use(blogRoute)
+
+app.use(express.static(path.join(__dirname,'build')))
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+
+
+
+
+
+
+
